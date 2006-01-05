a0fffc89f80ce2b0a16a7617193dda79ca00f12d
markm <markm@34ed7ecb-0a45-3640-94b6-9dce24991b9f>
2006-01-05 18:19:36 +0000

    Initial import

---
+++ setup.py
@@ -0,0 +1,33 @@
+# GUI Application automation and testing library
+# Copyright (C) 2006 Mark Mc Mahon
+#
+# This library is free software; you can redistribute it and/or 
+# modify it under the terms of the GNU Lesser General Public License 
+# as published by the Free Software Foundation; either version 2.1 
+# of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public 
+# License along with this library; if not, write to the 
+#    Free Software Foundation, Inc.,
+#    59 Temple Place,
+#    Suite 330, 
+#    Boston, MA 02111-1307 USA 
+
+# to build files:
+# setup.py py2exe
+
+from distutils.core import setup 
+
+
+setup(name='Distutils',
+      version='0.1',
+      description='Python library for GUI automation and testing',
+      author='Mark Mc Mahon',
+      author_email='mark.m.mcmahon@gmail.com',
+      packages=['pywinauto'],
+     )
