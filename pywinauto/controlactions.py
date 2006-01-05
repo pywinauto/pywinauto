24686b7aec9a789eb84e90b661fad63a853fbca8
markm <markm@34ed7ecb-0a45-3640-94b6-9dce24991b9f>
2006-01-05 18:20:19 +0000

    Added License

--- pywinauto/controlactions.py
+++ pywinauto/controlactions.py
@@ -1,4 +1,23 @@
-#ControlActions
+# GUI Application automation and testing library
+# Copyright (C) 2006 Mark Mc Mahon
+#
+# This library is free software; you can redistribute it and/or 
+# modify it under the terms of the GNU Lesser General Public License 
+# as published by the Free Software Foundation; either version 2.1 
+# of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public 
+# License along with this library; if not, write to the 
+#    Free Software Foundation, Inc.,
+#    59 Temple Place,
+#    Suite 330, 
+#    Boston, MA 02111-1307 USA 
+
 import time
 
 from win32defines import *
@@ -130,8 +149,6 @@ def release_mouse_action(ctrl, button = "left", pressed = "", coords = (0, 0)):
 def move_mouse_action(ctrl, pressed = "left", coords = (0, 0)):
 	flags, click_point = calc_flags_and_coords(pressed, coords)	
 	ctrl.PostMessage(WM_MOUSEMOVE, flags, click_point)
-
-
 
 def settext_action(ctrl, text, append = False):
 	if append:
