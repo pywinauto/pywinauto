7866b3a9f27347eba0304633d191b416510bfb12
markm <markm@34ed7ecb-0a45-3640-94b6-9dce24991b9f>
2006-01-20 04:59:54 +0000

    ...

---
+++ makedocs.bat
@@ -0,0 +1,18 @@
+@echo off
+
+c:\.temp\pudge\pudge\cli --documents docs\about.rst,docs\index.rst --title pywinauto -v -d pudge_output_green_paste -m pywinauto.application,pywinauto.clipboard,pywinauto.controlactions,pywinauto.findbestmatch,pywinauto.findwindows,pywinauto.handleprops,pywinauto.XMLHelpers,pywinauto.controls,pywinauto.tests  -t \.temp\pudge\pudge\template\green_paste
+
+REM --documents docs\about.rst
+
+REM c:\.temp\pudge\pudge\cli --title pywinauto -v -d pudge_output_green -m pywinauto.application,pywinauto.clipboard,pywinauto.controlactions,pywinauto.findbestmatch,pywinauto.findwindows,pywinauto.handleprops,pywinauto.XMLHelpers,pywinauto.controls,pywinauto.tests  -t \.temp\pudge\pudge\template\green
+
+REM c:\.temp\pudge\pudge\cli --title pywinauto -v -d pudge_output_lesscode -m pywinauto.application,pywinauto.clipboard,pywinauto.controlactions,pywinauto.findbestmatch,pywinauto.findwindows,pywinauto.handleprops,pywinauto.XMLHelpers,pywinauto.controls,pywinauto.tests  -t \.temp\pudge\pudge\template\lesscode.org
+REM c:\.temp\pudge\pudge\cli --title pywinauto -v -d pudge_output_base -m pywinauto.application,pywinauto.clipboard,pywinauto.controlactions,pywinauto.findbestmatch,pywinauto.findwindows,pywinauto.handleprops,pywinauto.XMLHelpers,pywinauto.controls,pywinauto.tests  -t \.temp\pudge\pudge\template\base
+REM c:\.temp\pudge\pudge\cli --title pywinauto -v -d pudge_output_pythonpaste -m pywinauto.application,pywinauto.clipboard,pywinauto.controlactions,pywinauto.findbestmatch,pywinauto.findwindows,pywinauto.handleprops,pywinauto.XMLHelpers,pywinauto.controls,pywinauto.tests  -t \.temp\pudge\pudge\template\pythonpaste.org
+
+REM pywinauto.win32defines
+REM pywinauto.win32functions
+REM pywinauto.win32structures
+
+
+REM pywinauto.application, pywinauto.clipboard, pywinauto.controlactions, pywinauto.findbestmatch, pywinauto.findwindows, pywinauto.handleprops, pywinauto.XMLHelpers, pywinauto.controls, pywinauto.tests
