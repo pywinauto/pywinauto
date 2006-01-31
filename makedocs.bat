689a656b05531ffda7d643fac01f9ab66765824c
markm <markm@34ed7ecb-0a45-3640-94b6-9dce24991b9f>
2006-01-31 19:14:06 +0000

    Minor Doc Changes

--- makedocs.bat
+++ makedocs.bat
@@ -1,8 +1,8 @@
 @echo off
 
-c:\.temp\pudge\pudge\cli --documents docs\about.rst,docs\index.rst,history.txt,license.txt,todo.txt --title pywinauto -v -d pudge_output_green_paste -m pywinauto.application,pywinauto.clipboard,pywinauto.controlactions,pywinauto.findbestmatch,pywinauto.findwindows,pywinauto.handleprops,pywinauto.XMLHelpers,pywinauto.controls,pywinauto.tests  -t \.temp\pudge\pudge\template\green_paste
+c:\.temp\pudge\pudge\cli --documents docs\index.rst,history.txt,license.txt,todo.txt,docs\howto.rst --title pywinauto -v -d pudge_output_green_paste -m pywinauto.application,pywinauto.clipboard,pywinauto.controlactions,pywinauto.findbestmatch,pywinauto.findwindows,pywinauto.handleprops,pywinauto.XMLHelpers,pywinauto.controls,pywinauto.tests  -t \.temp\pudge\pudge\template\green_paste
 
-REM --documents docs\about.rst
+REM docs\about.rst
 
 REM c:\.temp\pudge\pudge\cli --title pywinauto -v -d pudge_output_green -m pywinauto.application,pywinauto.clipboard,pywinauto.controlactions,pywinauto.findbestmatch,pywinauto.findwindows,pywinauto.handleprops,pywinauto.XMLHelpers,pywinauto.controls,pywinauto.tests  -t \.temp\pudge\pudge\template\green
 
