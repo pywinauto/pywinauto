d42fda43964c2e1fc7c54270c0623e45c034155e
markm <markm@34ed7ecb-0a45-3640-94b6-9dce24991b9f>
2006-03-06 20:02:00 +0000

    Implemented more tests

--- pywinauto/unittests/test_application.py
+++ pywinauto/unittests/test_application.py
@@ -29,8 +29,12 @@ import time
 import pprint
 import pdb
 
+from pywinauto import application
 from pywinauto.application import *
 from pywinauto import findwindows
+
+
+application.set_timing(1, .01, 1, .01, .05, 0, 0, .1, 0, .01)
 
 class ApplicationTestCases(unittest.TestCase):
     "Unit tests for the application.Application class"
@@ -76,12 +80,45 @@ class ApplicationTestCases(unittest.TestCase):
 
         app = Application()
         self.assertEqual(app.process, None)
+        application.app_start_timeout = 1
         app_name = r"I am not * and Application!/\.exe"
         try:
             app.start_(app_name)
         except AppStartError, e:
             self.assertEquals(app_name in str(e), True)
 
+
+    def testset_timing(self):
+        from pywinauto.controls import HwndWrapper
+        prev_timing = (
+            application.window_find_timeout,
+            application.window_retry_interval,
+            application.app_start_timeout,
+            application.exists_timeout,
+            application.exists_retry_interval,
+            HwndWrapper.delay_after_click,
+            HwndWrapper.delay_after_menuselect,
+            HwndWrapper.delay_after_sendkeys_key,
+            HwndWrapper.delay_after_button_click,
+            HwndWrapper.delay_before_after_close_click,
+        )
+        set_timing(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+        self.assertEquals(
+            (
+                application.window_find_timeout,
+                application.window_retry_interval,
+                application.app_start_timeout,
+                application.exists_timeout,
+                application.exists_retry_interval,
+                HwndWrapper.delay_after_click,
+                HwndWrapper.delay_after_menuselect,
+                HwndWrapper.delay_after_sendkeys_key,
+                HwndWrapper.delay_after_button_click,
+                HwndWrapper.delay_before_after_close_click,
+            ), (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) )
+
+        set_timing(*prev_timing)
 
 
 
@@ -216,13 +253,13 @@ class ApplicationTestCases(unittest.TestCase):
         self.assertEqual(title.handle, handle.handle)
         self.assertEqual(title.handle, bestmatch.handle)
 
-
         app.UntitledNotepad.MenuSelect("File->Exit")
 
     def testGetitem(self):
         app = Application()
         app.start_('notepad.exe')
 
+        application.window_find_timeout = .5
         try:
             app['blahblah']
         except:
@@ -284,39 +321,184 @@ class WindowSpecificationTestCases(unittest.TestCase):
         """Start the application set some data and ensure the application
         is in the state we want it."""
         self.app = Application().start_("Notepad")
-        self.app.UntitledNotepad.MenuSelect("File->PageSetup")
+        self.dlgspec = self.app.UntitledNotepad
+        self.ctrlspec = self.app.UntitledNotepad.Edit
+
 
     def tearDown(self):
         "Close the application after tests"
         # close the application
-        self.app.PageSetup.Cancel.CloseClick()
         self.app.UntitledNotepad.MenuSelect("File->Exit")
 
 
-#    def testInit(self):
-#        "Make sure the friendly class is set correctly"
-#        pass
-#
-#    def testCtrl(self):
-#        "Make sure the friendly class is set correctly"
-#        pass
-#
-#    def testWindow(self):
-#        "Make sure the friendly class is set correctly"
-#        pass
-#
-#    def testGetItem(self):
-#        "Make sure the friendly class is set correctly"
-#        pass
-#
-#    def testGetAttr(self):
-#        "Make sure the friendly class is set correctly"
-#        pass
-#
-#    def testExists(self):
-#        "Make sure the friendly class is set correctly"
-#        pass
-#
+    def test__init__(self):
+        "Test creating a new spec by hand"
+
+        wspec = WindowSpecification(
+            dict(
+                best_match = u"UntitledNotepad",
+                process = self.app.process)
+            )
+
+        self.assertEquals(
+            wspec.WindowText(),
+            u"Untitled - Notepad")
+
+
+    def test__call__(self):
+        self.assertRaises(AttributeError, self.dlgspec)
+        self.assertRaises(AttributeError, self.ctrlspec)
+
+        # no best_match!
+        wspec = WindowSpecification(
+            dict(title = u"blah",) )
+
+        self.assertRaises(AttributeError, wspec)
+
+
+
+    def testCtrl(self):
+        "Test that we can get a control "
+        from pywinauto.controls import HwndWrapper
+        self.assertEquals(True, isinstance(self.dlgspec, WindowSpecification))
+
+        self.assertEquals(
+            True,
+            isinstance(self.dlgspec.ctrl_(), HwndWrapper.HwndWrapper)
+            )
+
+    def testWindow(self):
+        "test specifying a sub window of an existing specification"
+        sub_spec = self.dlgspec.window_(class_name = "Edit")
+
+        self.assertEquals(True, isinstance(sub_spec, WindowSpecification))
+        self.assertEquals(sub_spec.Class(), "Edit")
+
+
+
+    def test__getitem__(self):
+        "test item access of a windowspec"
+
+        self.assertEquals(
+            True,
+            isinstance(self.dlgspec['Edit'], WindowSpecification)
+            )
+
+        self.assertEquals(self.dlgspec['Edit'].Class(), "Edit")
+
+        self.assertRaises(AttributeError, self.ctrlspec.__getitem__, 'edit')
+
+
+
+    def testGetAttr(self):
+        "Make sure the friendly class is set correctly"
+        self.assertEquals(
+            True,
+            isinstance(self.dlgspec.Edit, WindowSpecification)
+            )
+        self.assertEquals(self.dlgspec.Edit.Class(), "Edit")
+
+
+
+    def testExists(self):
+        "Check that windows exist"
+
+        self.assertEquals(True, self.dlgspec.Exists())
+        self.assertEquals(True, self.dlgspec.Exists(0))
+        self.assertEquals(True, self.ctrlspec.Exists())
+        self.assertEquals(True, self.app.DefaultIME.Exists())
+
+        self.assertEquals(False, self.app.BlahBlah.Exists(.1))
+
+
+    def testExists_timing(self):
+
+        # try ones that should be found immediately
+        start = time.time()
+        self.assertEquals(True, self.dlgspec.Exists())
+        self.assertEquals(True, time.time() - start < .1)
+
+        start = time.time()
+        self.assertEquals(True, self.ctrlspec.Exists())
+        self.assertEquals(True, time.time() - start < .1)
+
+        # try one that should not be found
+        start = time.time()
+        self.assertEquals(True, self.dlgspec.Exists(.5))
+        timedif =  time.time() - start
+        self.assertEquals(True, .49 > timedif < .6)
+
+
+    def testWait(self):
+
+        allowable_error = .02
+
+        start = time.time()
+        self.assertEqual(self.dlgspec.ctrl_(), self.dlgspec.Wait("enaBleD "))
+        self.assertEqual(True, 0 <= (time.time() - start) < 0 + allowable_error)
+
+        start = time.time()
+        self.assertEqual(self.dlgspec.ctrl_(), self.dlgspec.Wait("  ready"))
+        self.assertEqual(True, 0 <= (time.time() - start) < 0 + allowable_error)
+
+        start = time.time()
+        self.assertEqual(self.dlgspec.ctrl_(), self.dlgspec.Wait(" exiSTS"))
+        self.assertEqual(True, 0 <= (time.time() - start) < 0 + allowable_error)
+
+        start = time.time()
+        self.assertEqual(self.dlgspec.ctrl_(), self.dlgspec.Wait(" VISIBLE "))
+        self.assertEqual(True, 0 <= (time.time() - start) < 0 + allowable_error)
+
+        start = time.time()
+        self.assertEqual(self.dlgspec.ctrl_(), self.dlgspec.Wait(" ready enabled"))
+        self.assertEqual(True, 0 <= (time.time() - start) < 0 + allowable_error)
+
+        start = time.time()
+        self.assertEqual(self.dlgspec.ctrl_(), self.dlgspec.Wait("visible exists "))
+        self.assertEqual(True, 0 <= (time.time() - start) < 0 + allowable_error)
+
+        start = time.time()
+        self.assertEqual(self.dlgspec.ctrl_(), self.dlgspec.Wait("exists "))
+        self.assertEqual(True, 0 <= (time.time() - start) < 0 + allowable_error)
+
+
+
+    def testWaitNot(self):
+        """Test that wait not fails for all the following
+
+        raises and error when criteria not met
+        timing is close to the timeout value"""
+        allowable_error = .02
+
+        start = time.time()
+        self.assertRaises(RuntimeError, self.dlgspec.WaitNot, "enaBleD ", .1, .05)
+        self.assertEqual(True, .1 <= (time.time() - start) < .1 + allowable_error)
+
+        start = time.time()
+        self.assertRaises(RuntimeError, self.dlgspec.WaitNot, "  ready", .1, .05)
+        self.assertEqual(True, .1 <= (time.time() - start) < .1 + allowable_error)
+
+        start = time.time()
+        self.assertRaises(RuntimeError, self.dlgspec.WaitNot, " exiSTS", .1, .05)
+        self.assertEqual(True, .1 <= (time.time() - start) < .1 + allowable_error)
+
+        start = time.time()
+        self.assertRaises(RuntimeError, self.dlgspec.WaitNot, " VISIBLE ", .1, .05)
+        self.assertEqual(True, .1 <= (time.time() - start) < .1 + allowable_error)
+
+        start = time.time()
+        self.assertRaises(RuntimeError, self.dlgspec.WaitNot, " ready enabled", .1, .05)
+        self.assertEqual(True, .1 <= (time.time() - start) < .1 + allowable_error)
+
+        start = time.time()
+        self.assertRaises(RuntimeError, self.dlgspec.WaitNot, "visible exists ", .1, .05)
+        self.assertEqual(True, .1 <= (time.time() - start) < .1 + allowable_error)
+
+        start = time.time()
+        self.assertRaises(RuntimeError, self.dlgspec.WaitNot, "exists ", .1, .05)
+        self.assertEqual(True, .1 <= (time.time() - start) < .1 + allowable_error)
+
+
 #    def testWaitReady(self):
 #        "Make sure the friendly class is set correctly"
 #        pass
--- pywinauto/unittests/test_handleprops.py
+++ pywinauto/unittests/test_handleprops.py
@@ -25,36 +25,211 @@ __revision__ = "$Revision: 234 $"
 import unittest
 
 from pywinauto.handleprops import *
+from pywinauto.application import Application
 
-#class ApplicationTestCases(unittest.TestCase):
-#    "Unit tests for the ListViewWrapper class"
-#
-#    def setUp(self):
-#        """Start the application set some data and ensure the application
-#        is in the state we want it."""
-#        pass
-#
-#    def tearDown(self):
-#        "Close the application after tests"
-#        # close the application
-#        #self.dlg.SendMessage(win32defines.WM_CLOSE)
-#        pass
-#
-#    def testNotConnected(self):
-#        "Make sure the friendly class is set correctly"
-#        self.assertRaises (AppNotConnected, Application().__getattr__, 'Hiya')
-#        self.assertRaises (AppNotConnected, Application().__getitem__, 'Hiya')
-#        self.assertRaises (AppNotConnected, Application().window_, title = 'Hiya')
-#        self.assertRaises (AppNotConnected, Application().top_window_,)
-#
-#    def testStartProplem(self):
-#        "Make sure the friendly class is set correctly"
-#        self.assertRaises (AppStartError, Application().start_, 'Hiya')
-#
-#    #def testStartProplem(self):
-#    #    "Make sure the friendly class is set correctly"
-#    #    self.assertRaises (AppStartError, Application().start_, 'Hiya')
-#
+
+
+
+class handlepropsTestCases(unittest.TestCase):
+    "Unit tests for the handleprops module"
+
+    def setUp(self):
+        """Start the application set some data and ensure the application
+        is in the state we want it."""
+        self.app = Application().start_("notepad")
+        self.dlghandle = self.app.UntitledNotepad.handle
+        self.edit_handle = self.app.UntitledNotepad.Edit.handle
+
+    def tearDown(self):
+        "Close the application after tests"
+        # close the application
+        #self.dlg.SendMessage(win32defines.WM_CLOSE)
+        self.app.UntitledNotepad.MenuSelect("File->Exit")
+
+    def test_text(self):
+        "Make sure the text method returns correct result"
+        self.assertEquals("Untitled - Notepad", text(self.dlghandle))
+        self.assertEquals("", text(self.edit_handle))
+
+
+    def test_classname(self):
+        "Make sure the classname method returns correct result"
+        self.assertEquals("Notepad", classname(self.dlghandle))
+        self.assertEquals("Edit", classname(self.edit_handle))
+
+    def test_parent(self):
+        "Make sure the parent method returns correct result"
+        self.assertEquals(0, parent(self.dlghandle))
+        self.assertEquals(self.dlghandle, parent(self.edit_handle))
+
+    def test_style(self):
+        "Make sure the style method returns correct result"
+        self.assertEquals(0x14cf0000, style(self.dlghandle))
+        self.assertEquals(0x50300104, style(self.edit_handle))
+
+    def test_exstyle(self):
+        "Make sure the exstyle method returns correct result"
+        self.assertEquals(0x110, exstyle(self.dlghandle))
+        self.assertEquals(0x200, exstyle(self.edit_handle))
+
+    def test_controlid(self):
+        "Make sure the controlid method returns correct result"
+        #self.assertEquals(0, controlid(self.dlghandle))
+        self.assertEquals(15, controlid(self.edit_handle))
+
+    def test_userdata(self):
+        "Make sure the userdata method returns correct result"
+        self.assertEquals(0, userdata(self.dlghandle))
+        self.assertEquals(0, userdata(self.edit_handle))
+
+    def test_contexthelpid(self):
+        "Make sure the contexthelpid method returns correct result"
+        self.assertEquals(0, contexthelpid(self.dlghandle))
+        self.assertEquals(0, contexthelpid(self.edit_handle))
+
+    def test_iswindow(self):
+        "Make sure the iswindow method returns correct result"
+        self.assertEquals(True, iswindow(self.dlghandle))
+        self.assertEquals(True, iswindow(self.edit_handle))
+
+        self.assertEquals(False, iswindow(1))
+
+    def test_isvisible(self):
+        "Make sure the isvisible method returns correct result"
+        self.assertEquals(True, isvisible(self.dlghandle))
+        self.assertEquals(True, isvisible(self.edit_handle))
+
+        # need to check something invisible
+        #self.assertEquals(False, isvisible(self.edit_handle))
+
+    def test_isunicode(self):
+        "Make sure the isunicode method returns correct result"
+        self.assertEquals(True, isunicode(self.dlghandle))
+        self.assertEquals(True, isunicode(self.edit_handle))
+
+        # need to check something not unicode
+        #self.assertEquals(False, isunicode(self.edit_handle))
+
+
+    def test_isenabled(self):
+        "Make sure the isenabled method returns correct result"
+        self.assertEquals(True, isenabled(self.dlghandle))
+        self.assertEquals(True, isenabled(self.edit_handle))
+
+        self.app.UntitledNotepad.MenuSelect("Format->Font")
+        self.assertEquals(False, isenabled(self.dlghandle))
+        self.app.Font.Cancel.CloseClick()
+
+        self.app.UntitledNotepad.MenuSelect("Edit->Replace")
+        self.assertEquals(
+            False,
+            isenabled(
+                self.app.Replace.window_(
+                    title_re = "Replace.*",
+                    class_name = "Button",
+                    enabled_only = False).handle))
+        self.app.Replace.Cancel.Click()
+
+    def test_clientrect(self):
+        "Make sure the friendly class is set correctly"
+        self.assertEquals(0, clientrect(self.dlghandle).left)
+        self.assertEquals(0, clientrect(self.edit_handle).left)
+
+        self.assertEquals(0, clientrect(self.dlghandle).top)
+        self.assertEquals(0, clientrect(self.edit_handle).top)
+
+        self.assertEquals(True,
+            rectangle(self.dlghandle).right > clientrect(self.dlghandle).right)
+        self.assertEquals(True,
+            rectangle(self.edit_handle).right > clientrect(self.edit_handle).right)
+
+        self.assertEquals(True,
+            rectangle(self.dlghandle).bottom > clientrect(self.dlghandle).bottom)
+        self.assertEquals(True,
+            rectangle(self.edit_handle).bottom > clientrect(self.edit_handle).bottom)
+
+
+
+    def test_rectangle(self):
+        "Make sure the friendly class is set correctly"
+        dlgrect = rectangle(self.dlghandle)
+        self.assertEquals(True, dlgrect.left < dlgrect.right)
+        self.assertEquals(True, dlgrect.top < dlgrect.bottom)
+
+        editrect = rectangle(self.edit_handle)
+        self.assertEquals(True, editrect.left < editrect.right)
+        self.assertEquals(True, editrect.top < editrect.bottom)
+
+    def test_font(self):
+        "Make sure the friendly class is set correctly"
+        dlgfont = font(self.dlghandle)
+        self.assertEquals(True, isinstance(dlgfont.lfFaceName, basestring))
+
+        editfont = font(self.edit_handle)
+        self.assertEquals(True, isinstance(editfont.lfFaceName, basestring))
+
+
+    def test_processid(self):
+        "Make sure the friendly class is set correctly"
+        self.assertEquals(self.app.process, processid(self.dlghandle))
+        self.assertEquals(self.app.process, processid(self.edit_handle))
+
+    def test_children(self):
+        "Make sure the children method returns correct result"
+        self.assertEquals(2,  len(children(self.dlghandle)))
+        self.assertEquals([], children(self.edit_handle))
+
+    def test_has_style(self):
+        "Make sure the has_style method returns correct result"
+        self.assertEquals(True,  has_style(self.dlghandle, 0xf0000))
+        self.assertEquals(True, has_style(self.edit_handle, 0x4))
+
+        self.assertEquals(False,  has_style(self.dlghandle, 4))
+        self.assertEquals(False, has_style(self.edit_handle, 1))
+
+
+    def test_has_exstyle(self):
+        "Make sure the has_exstyle method returns correct result"
+        self.assertEquals(True,  has_exstyle(self.dlghandle, 0x10))
+        self.assertEquals(True, has_exstyle(self.edit_handle, 0x200))
+
+        self.assertEquals(False,  has_exstyle(self.dlghandle, 4))
+        self.assertEquals(False, has_exstyle(self.edit_handle, 0x10))
+
+    def test_is_toplevel_window(self):
+        "Make sure the friendly class is set correctly"
+
+        self.assertEquals(True, is_toplevel_window(self.dlghandle))
+        self.assertEquals(False, is_toplevel_window(self.edit_handle))
+
+        self.app.UntitledNotepad.MenuSelect("Edit->Replace")
+        self.assertEquals(True, is_toplevel_window(self.app.Replace.handle))
+        self.assertEquals(False, is_toplevel_window(self.app.Replace.Cancel.handle))
+        self.app.Replace.Cancel.Click()
+
+    def test_friendlyclassname(self):
+        "Make sure the friendly class is returned correctly"
+        self.assertEquals("Notepad", friendlyclassname(self.dlghandle))
+        self.assertEquals("Edit", friendlyclassname(self.edit_handle))
+
+        self.app.UntitledNotepad.MenuSelect("Edit->Replace")
+        self.assertEquals("Dialog", friendlyclassname(self.app.Replace.handle))
+        self.app.Replace.Cancel.Click()
+
+
+    def test_dumpwindow(self):
+        "Make sure the friendly class is set correctly"
+        dlgdump = dumpwindow(self.dlghandle)
+
+        for key, item in dlgdump.items():
+            self.assertEquals(item, globals()[key](self.dlghandle))
+
+        editdump = dumpwindow(self.edit_handle)
+
+        for key, item in editdump.items():
+            self.assertEquals(item, globals()[key](self.edit_handle))
+
+
 
 
 if __name__ == "__main__":
