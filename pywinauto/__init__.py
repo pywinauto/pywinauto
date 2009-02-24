a4acca936c3ad2c135170830f733c25c5f5760fe
markm <markm@34ed7ecb-0a45-3640-94b6-9dce24991b9f>
2009-02-24 22:33:41 +0000

    * increase the version number

--- pywinauto/__init__.py
+++ pywinauto/__init__.py
@@ -23,7 +23,7 @@ Python package for automating GUI manipulation on Windows
 
 """
 __revision__ = "$Revision$"
-__version__ = "0.3.7"
+__version__ = "0.3.8"
 
 import findwindows
 WindowAmbiguousError = findwindows.WindowAmbiguousError
