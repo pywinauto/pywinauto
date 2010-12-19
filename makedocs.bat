a941bcfa3f93c3e7188fdac65e23777fd816d547
markm <markm@localhost>
2010-12-19 17:52:10 +0900

    Fix the path to build_autodoc_files.py and call it by default everytime (it is safe as it does not re-generate files which already exist)

--- makedocs.bat
+++ makedocs.bat
@@ -1,7 +1,7 @@
 @echo off
 
 
-REM docs\build_autodoc_files.py
+doc_src\build_autodoc_files.py
 sphinx-build -b html doc_src pywinauto\docs
 
 REM c:\.temp\pudge\pudge\cli  --documents doc_source\index.rst,doc_source\controls_overview.rst,doc_source\howto.rst,doc_source\getting_started.rst,history.txt,license.txt,todo.txt --title pywinauto  -v  -d website  -m pywinauto.application,pywinauto.taskbar,pywinauto.clipboard,pywinauto.timings,pywinauto.findbestmatch,pywinauto.findwindows,pywinauto.handleprops,pywinauto.XMLHelpers,pywinauto.controls,pywinauto.tests   -t doc_source\template
