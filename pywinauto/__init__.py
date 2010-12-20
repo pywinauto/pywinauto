09985bcaad408085ae0c3c0fc3ca9b5639e487a8
markm <markm@localhost>
2010-12-20 17:39:00 +0900

    Bump the revision number for the next release

--- pywinauto/__init__.py
+++ pywinauto/__init__.py
@@ -23,7 +23,7 @@ Python package for automating GUI manipulation on Windows
 
 """
 __revision__ = "$Revision$"
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 import findwindows
 WindowAmbiguousError = findwindows.WindowAmbiguousError
