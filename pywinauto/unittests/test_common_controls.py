2552b1154d4f80e599f612c4760a6d6a224ea095
Valentin Kroupkin <assotut@gmail.com>
2015-11-15 18:06:20 +1300

    test_common_controls: double click in testAfxToolBarButtons

--- pywinauto/unittests/test_common_controls.py
+++ pywinauto/unittests/test_common_controls.py
@@ -1251,7 +1251,7 @@ class RebarTestCases(unittest.TestCase):
         "Make sure we can click on Afx ToolBar button by index"
         self.dlg.StandardToolbar.Button(1).Click()
         self.app.Window_(title='Open').Wait('ready')
-        self.app.Window_(title='Open').Cancel.CloseClick()
+        self.app.Window_(title='Open').Cancel.CloseClick(double=True)
 
     def testMenuBarClickInput(self):
         "Make sure we can click on Menu Bar items by indexed path"
