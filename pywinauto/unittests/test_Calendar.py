741b19bbdd9b47f48b4f32f148ae426b54af1a58
Anna Ryabusheva <anna.ryabusheva@smartstepgroup.com>
2016-04-19 01:22:01 -0500

    Added tests on faild to set date and view

--- pywinauto/unittests/test_Calendar.py
+++ pywinauto/unittests/test_Calendar.py
@@ -1,4 +1,4 @@
-"Tests for HwndWrapper"
+"Tests for CalendarWrapper"
 
 import time
 import datetime
@@ -52,10 +52,13 @@ class CalendarWrapperTests(unittest.TestCase):
         date = self.calendar.GetCurrentDate()
         self.assertThatSystemTimeIsEqualCurrentDateTime(date,datetime.date.today())
 
-    def testCanSetCurrenDateInCalendar(self):       
+    def testCanSetCurrenDateInCalendar(self):
         self.calendar.SetDate(2016, 4, 3, 13)
         self.assertThatSystemTimeIsEqualCurrentDateTime(self.calendar.GetCurrentDate(),
                                                 datetime.date(2016, 4, 13)) 
+
+    def testShouldReturnErrorwhenTryToSetInvalidDate(self):
+        self.assertRaises(RuntimeError, self.calendar.SetDate, -2016, -4, -3, -13)
 
     def testCanGetCalendarBorder(self):
         width = self.calendar.GetCalendarBorderWidth()
@@ -73,6 +76,9 @@ class CalendarWrapperTests(unittest.TestCase):
         view = self.calendar.GetCalendarView()
         self.assertEqual(view, 0)
 
+    def testShouldReturnErrorwhenTryToSetInvalidView(self):
+        self.assertRaises(RuntimeError, self.calendar.SetCalendarView, -1)
+
     def testCanSetCalendarsViewIntoMonth(self):
         self.calendar.SetCalendarView(win32defines.MCMV_MONTH)
         self.assertEqual(self.calendar.GetCalendarView(), win32defines.MCMV_MONTH)
@@ -89,7 +95,7 @@ class CalendarWrapperTests(unittest.TestCase):
         self.calendar.SetCalendarView(win32defines.MCMV_CENTURY)
         self.assertEqual(self.calendar.GetCalendarView(), win32defines.MCMV_CENTURY)
 
-    def assertThatSystemTimeIsEqualCurrentDateTime(self,systemTime, now):        
+    def assertThatSystemTimeIsEqualCurrentDateTime(self,systemTime, now):
         self.assertEqual(systemTime.wYear, now.year)
         self.assertEqual(systemTime.wMonth, now.month)
         self.assertEqual(systemTime.wDay, now.day)
