167acb8dc3c85f3a0a741e82808b1ebb373e1cf9
Oleg Kainov <kainov.oleg@gmail.com>
2016-04-19 19:08:11 +0400

    Removed unused imports

--- doc_src/conf.py
+++ doc_src/conf.py
@@ -12,10 +12,6 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
-import importlib
-import imp
-import platform
-import sys
 import os
 import re
 
