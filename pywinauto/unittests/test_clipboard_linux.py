04be7911d086306408f420a35f2b4de500328f05
Ivan Magazinnik <magazinnik.ivan@gmail.com>
2016-10-28 20:39:11 +0300

    disable test run on windows

--- pywinauto/unittests/test_clipboard_linux.py
+++ pywinauto/unittests/test_clipboard_linux.py
@@ -26,43 +26,43 @@ def _test_app():
     return os.path.join(test_folder, r"send_keys_test_app")
 
 
-class SendKeysTests(unittest.TestCase):
-    """Unit tests for the linux clipboard module"""
+if sys.platform != 'win32':
+    class SendKeysTests(unittest.TestCase):
+        """Unit tests for the linux clipboard module"""
 
-    def setUp(self):
-        """Start the application set some data and ensure the application is in the state we want it."""
-        self.app = subprocess.Popen("exec " + _test_app(), shell=True)
-        time.sleep(0.1)
-        mouse.click(coords=(300, 300))
-        time.sleep(0.1)
+        def setUp(self):
+            """Start the application set some data and ensure the application is in the state we want it."""
+            self.app = subprocess.Popen("exec " + _test_app(), shell=True)
+            time.sleep(0.1)
+            mouse.click(coords=(300, 300))
+            time.sleep(0.1)
 
-    def tearDown(self):
-        """Close the application after tests"""
-        self.app.kill()
+        def tearDown(self):
+            """Close the application after tests"""
+            self.app.kill()
 
-    def receive_text(self):
-        """Receive data from text field"""
-        time.sleep(0.2)
-        SendKeys('^a')
-        time.sleep(0.2)
-        SendKeys('^c')
-        SendKeys('{RIGHT}')
-        received = clipboard.get_data()
-        return received
+        def receive_text(self):
+            """Receive data from text field"""
+            time.sleep(0.2)
+            SendKeys('^a')
+            time.sleep(0.2)
+            SendKeys('^c')
+            SendKeys('{RIGHT}')
+            received = clipboard.get_data()
+            return received
 
-    def test_get_data(self):
-        """Make sure that get text from clipboard works"""
-        SendKeys('abc')
-        received = self.receive_text()
-        self.assertEquals('abc', received)
+        def test_get_data(self):
+            """Make sure that get text from clipboard works"""
+            SendKeys('abc')
+            received = self.receive_text()
+            self.assertEquals('abc', received)
 
-    def test_set_data(self):
-        """Make sure that set text to clipboard works"""
-        clipboard.set_data('abc1')
-        received = clipboard.get_data()
-        self.assertEquals('abc1', received)
+        def test_set_data(self):
+            """Make sure that set text to clipboard works"""
+            clipboard.set_data('abc1')
+            received = clipboard.get_data()
+            self.assertEquals('abc1', received)
 
 
 if __name__ == "__main__":
-    if sys.platform != 'win32':
         unittest.main()
