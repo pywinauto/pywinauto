3e92842b2f9f683e7221bce3c13409703e453c71
Ivan Magazinnik <magazinnik.ivan@gmail.com>
2016-10-28 19:00:59 +0300

    fix import disable on windows

--- pywinauto/unittests/test_clipboard_linux.py
+++ pywinauto/unittests/test_clipboard_linux.py
@@ -1,19 +1,21 @@
 """Module containing tests for linux clipboard module"""
+
 import sys
 import os
 import unittest
 import subprocess
 import time
+if sys.platform != 'win32':
+    sys.path.append(".")
+    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.insert(0, parent_dir)
+    import mouse
 
-sys.path.append(".")
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, parent_dir)
-import mouse
+    send_keys_dir = os.path.join(parent_dir, r"linux")
+    sys.path.insert(0, send_keys_dir)
+    from pywinauto.keyboard import SendKeys
+    import clipboard
 
-send_keys_dir = os.path.join(parent_dir, r"linux")
-sys.path.insert(0, send_keys_dir)
-from pywinauto.keyboard import SendKeys, KeySequenceError, KeyAction
-import clipboard
 
 def _test_app():
     test_folder = os.path.join(os.path.dirname
