9d688ee0185723a800cb2337d9ef90cbe6a6dbf4
MagazinnikIvan <magazinnik.ivan@gmail.com>
2017-02-27 19:36:41 +0300

    fix rectangle property

--- pywinauto/atspi_functions.py
+++ pywinauto/atspi_functions.py
@@ -1,13 +1,20 @@
 import ctypes
-from ctypes import c_char_p, c_int
+from ctypes import Structure, c_char_p, c_int
 
 
-class AtspiRect (ctypes.Structure):
+class AtspiRect (Structure):
     _fields_ = [
         ('x', c_int),
         ('y', c_int),
         ('width', c_int),
         ('height', c_int),
+    ]
+
+
+class AtspiPoint (Structure):
+    _fields_ = [
+        ('x', c_int),
+        ('y', c_int),
     ]
 
 
@@ -23,7 +30,9 @@ get_parent = atspi.atspi_accessible_get_parent
 get_child_count = atspi.atspi_accessible_get_child_count
 get_child_at_index = atspi.atspi_accessible_get_child_at_index
 get_position = atspi.atspi_component_get_position
+get_position.restype = AtspiPoint
 get_size = atspi.atspi_component_get_size
+get_size.restype = AtspiPoint
 get_rectangle = atspi.atspi_component_get_extents
-get_rectangle.restype = AtspiRect
-get_component = atspi.atspi_accessible_get_component_iface
+get_rectangle.restype = ctypes.POINTER(AtspiRect)
+get_component = atspi.atspi_accessible_get_component
