4be48fed23c2e5bc6a8b19357e101eeb8f69c590
cetygamer <cetygamer@gmail.com>
2017-05-21 20:57:03 +0300

    improve speed of ControlTree

--- pywinauto/script_generator/control_tree.py
+++ pywinauto/script_generator/control_tree.py
@@ -32,27 +32,21 @@ class ControlTree(object):
         """Create tree structure"""
         # Create a list of this control and all its descendants
         all_ctrls = [self.ctrl, ] + self.ctrl.descendants()
+        txt_ctrls = [ctrl for ctrl in all_ctrls if ctrl.can_be_label and ctrl.is_visible() and ctrl.window_text()]
 
-        # Build a disambiguated list of control names
-        name_control_map = findbestmatch.build_unique_dict(all_ctrls)
-
-        # Swap it around so that we are mapped off the controls
-        control_name_map = {}
-        for name, control in name_control_map.items():
-            control_name_map.setdefault(control, []).append(name)
-
-        self.root = ControlTreeNode(self.ctrl, control_name_map[self.ctrl], self.ctrl.rectangle())
-        self.root_name = [name for name in self.root.names if len(name) > 0 and not " " in name][-1]
+        root_names = findbestmatch.get_control_names(self.ctrl, all_ctrls, txt_ctrls)
+        self.root = ControlTreeNode(self.ctrl, root_names, self.ctrl.rectangle())
+        self.root_name = [name for name in root_names if len(name) > 0 and " " not in name][-1]
 
         def go_deep_down_the_tree(parent_node, child_ctrls, current_depth=1):
             if len(child_ctrls) == 0:
                 return
 
             for ctrl in child_ctrls:
-                if ctrl not in control_name_map.keys():
+                if ctrl not in all_ctrls:
                     continue
 
-                ctrl_names = control_name_map[ctrl]
+                ctrl_names = findbestmatch.get_control_names(ctrl, all_ctrls, txt_ctrls)
                 ctrl_rect = ctrl.rectangle()
 
                 ctrl_node = ControlTreeNode(ctrl, ctrl_names, ctrl_rect)
