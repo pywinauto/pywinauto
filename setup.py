0d8c808394dcf99653e6542b4d612ccbad52d56b
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2017-07-04 00:29:39 +0300

    Update project description on PyPI for future releases.

--- setup.py
+++ setup.py
@@ -79,6 +79,13 @@ setup(name='pywinauto',
 At it's simplest it allows you to send mouse and keyboard
 actions to windows dialogs and controls, but It has support for more complex
 controls also.
+
+Useful links
+-------------
+- Home page: http://pywinauto.github.io/
+- Docs Intro: https://pywinauto.readthedocs.io/en/latest/
+- Getting Started Guide: https://pywinauto.readthedocs.io/en/latest/getting_started.html
+- StackOverflow tag: https://stackoverflow.com/questions/tagged/pywinauto
 """,
     platforms=['win32'],
 
