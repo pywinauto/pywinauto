75674db5c612e37dc46c99a6d9ced62ab49cd224
airelil <assotut@gmail.com>
2017-09-30 11:38:24 +1300

    test_uiawrapper: fix import UIA specific comtypes module

--- pywinauto/unittests/test_uiawrapper.py
+++ pywinauto/unittests/test_uiawrapper.py
@@ -8,7 +8,6 @@ import sys
 import unittest
 import mock
 import six
-import comtypes
 
 sys.path.append(".")
 from pywinauto.application import Application, WindowSpecification  # noqa: E402
@@ -16,6 +15,7 @@ from pywinauto.sysinfo import is_x64_Python, UIA_support  # noqa: E402
 from pywinauto.timings import Timings  # noqa: E402
 from pywinauto.actionlogger import ActionLogger  # noqa: E402
 if UIA_support:
+    import comtypes
     import pywinauto.uia_defines as uia_defs
     import pywinauto.controls.uia_controls as uia_ctls
 
