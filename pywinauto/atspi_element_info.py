fce560825cf69da0b47d112cde3ddb816517e3c8
MagazinnikIvan <magazinnik.ivan@gmail.com>
2018-02-25 18:31:15 +0300

    add smart library search

--- pywinauto/atspi_element_info.py
+++ pywinauto/atspi_element_info.py
@@ -1,5 +1,4 @@
-from . import atspi_functions
-from .atspi_functions import AtspiRect, AtspiCoordType
+from .atspi_functions import AtspiRect, AtspiCoordType, AtspiFunctions
 from .element_info import ElementInfo
 
 
@@ -9,8 +8,9 @@ class AtspiElementInfo(ElementInfo):
 
     def __init__(self, handle=None):
         """Create element by handle (default is root element)"""
+        self.atspi_functions = AtspiFunctions()
         if handle is None:
-            self._handle = atspi_functions.get_desktop(0, None)
+            self._handle = self.atspi_functions.get_desktop(0, None)
         else:
             self._handle = handle
 
@@ -27,34 +27,34 @@ class AtspiElementInfo(ElementInfo):
     @property
     def name(self):
         """Return the text of the window"""
-        return atspi_functions.get_name(self._handle, None).decode(encoding='UTF-8')
+        return self.atspi_functions.get_name(self._handle, None).decode(encoding='UTF-8')
 
     @property
     def control_id(self):
         """Return the ID of the window"""
-        return atspi_functions.get_id(self._handle, None)
+        return self.atspi_functions.get_id(self._handle, None)
 
     @property
     def process_id(self):
         """Return the ID of process that controls this window"""
-        return atspi_functions.get_process_id(self._handle, None)
+        return self.atspi_functions.get_process_id(self._handle, None)
 
     @property
     def class_name(self):
         """Return the class name of the element"""
-        return atspi_functions.get_role_name(self._handle, None).decode(encoding='UTF-8')
+        return self.atspi_functions.get_role_name(self._handle, None).decode(encoding='UTF-8')
 
     @property
     def parent(self):
         """Return the parent of the element"""
-        return AtspiElementInfo(atspi_functions.get_parent(self._handle, None))
+        return AtspiElementInfo(self.atspi_functions.get_parent(self._handle, None))
 
     def children(self, **kwargs):
         """Return children of the element"""
-        len = atspi_functions.get_child_count(self._handle, None)
+        len = self.atspi_functions.get_child_count(self._handle, None)
         childrens = []
         for i in range(len):
-            childrens.append(atspi_functions.get_child_at_index(self._handle, i, None))
+            childrens.append(self.atspi_functions.get_child_at_index(self._handle, i, None))
         return [AtspiElementInfo(ch) for ch in childrens]
 
     def descendants(self, **kwargs):
@@ -67,6 +67,6 @@ class AtspiElementInfo(ElementInfo):
     @property
     def rectangle(self):
         """Return rectangle of element"""
-        component = atspi_functions.get_component(self._handle)
-        prect = atspi_functions.get_rectangle(component, AtspiCoordType.ATSPI_COORD_TYPE_SCREEN, None)
+        component = self.atspi_functions.get_component(self._handle)
+        prect = self.atspi_functions.get_rectangle(component, AtspiCoordType.ATSPI_COORD_TYPE_SCREEN, None)
         return prect.contents
