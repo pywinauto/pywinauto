716a1f4b7983f9fef08fda37c7e03f71df05f02e
MagazinnikIvan <magazinnik.ivan@gmail.com>
2018-05-27 14:45:01 +0300

    add RECT structure, fix remarks

--- pywinauto/linux/atspi_functions.py
+++ pywinauto/linux/atspi_functions.py
@@ -9,6 +9,118 @@ class CtypesEnum:
         return int(obj)
 
 
+class RECT(Structure):
+
+    """Wrap the RECT structure and add extra functionality"""
+
+    _fields_ = [
+        # C:/PROGRA~1/MIAF9D~1/VC98/Include/windef.h 287
+        ('left', c_int),
+        ('top', c_int),
+        ('right', c_int),
+        ('bottom', c_int),
+    ]
+
+    # ----------------------------------------------------------------
+    def __init__(self, otherRect_or_left = 0, top = 0, right = 0, bottom = 0):
+        """Provide a constructor for RECT structures
+
+        A RECT can be constructed by:
+        - Another RECT (each value will be copied)
+        - Values for left, top, right and bottom
+
+        e.g. my_rect = RECT(otherRect)
+        or   my_rect = RECT(10, 20, 34, 100)
+        """
+        if isinstance(otherRect_or_left, RECT):
+            self.left = otherRect_or_left.left
+            self.right = otherRect_or_left.right
+            self.top = otherRect_or_left.top
+            self.bottom = otherRect_or_left.bottom
+        elif isinstance(otherRect_or_left, AtspiRect):
+            self.left = otherRect_or_left.x
+            self.right = otherRect_or_left.x + otherRect_or_left.width
+            self.top = otherRect_or_left.y
+            self.bottom = otherRect_or_left.y + otherRect_or_left.height
+        else:
+            #if not isinstance(otherRect_or_left, (int, long)):
+            #    print type(self), type(otherRect_or_left), otherRect_or_left
+            self.left = otherRect_or_left
+            self.right = right
+            self.top = top
+            self.bottom = bottom
+
+
+#    # ----------------------------------------------------------------
+#    def __eq__(self, otherRect):
+#        "return true if the two rectangles have the same coordinates"
+#
+#        try:
+#            return \
+#                self.left == otherRect.left and \
+#                self.top == otherRect.top and \
+#                self.right == otherRect.right and \
+#                self.bottom == otherRect.bottom
+#        except AttributeError:
+#            return False
+
+    # ----------------------------------------------------------------
+    def __str__(self):
+        """Return a string representation of the RECT"""
+        return "(L%d, T%d, R%d, B%d)" % (
+            self.left, self.top, self.right, self.bottom)
+
+    # ----------------------------------------------------------------
+    def __repr__(self):
+        """Return some representation of the RECT"""
+        return "<RECT L%d, T%d, R%d, B%d>" % (
+            self.left, self.top, self.right, self.bottom)
+
+    # ----------------------------------------------------------------
+    def __sub__(self, other):
+        """Return a new rectangle which is offset from the one passed in"""
+        newRect = RECT()
+
+        newRect.left = self.left - other.left
+        newRect.right = self.right - other.left
+
+        newRect.top = self.top - other.top
+        newRect.bottom = self.bottom - other.top
+
+        return newRect
+
+    # ----------------------------------------------------------------
+    def __add__(self, other):
+        """Allow two rects to be added using +"""
+        newRect = RECT()
+
+        newRect.left = self.left + other.left
+        newRect.right = self.right + other.left
+
+        newRect.top = self.top + other.top
+        newRect.bottom = self.bottom + other.top
+
+        return newRect
+
+    # ----------------------------------------------------------------
+    def width(self):
+        """Return the width of the  rect"""
+        return self.right - self.left
+
+    # ----------------------------------------------------------------
+    def height(self):
+        """Return the height of the rect"""
+        return self.bottom - self.top
+
+    # ----------------------------------------------------------------
+    def mid_point(self):
+        """Return a POINT structure representing the mid point"""
+        pt = AtspiPoint()
+        pt.x = self.left + int(float(self.width())/2.)
+        pt.y = self.top + int(float(self.height())/2.)
+        return pt
+
+
 class AtspiCoordType(CtypesEnum):
     ATSPI_COORD_TYPE_SCREEN = 0
     ATSPI_COORD_TYPE_WINDOW = 1
