a99ce8076fe9b0f084ad4e4e8826f401a39cd2df
MagazinnikIvan <magazinnik.ivan@gmail.com>
2018-05-27 12:29:07 +0300

    use getattribute and getitem from base class

--- pywinauto/linux/application.py
+++ pywinauto/linux/application.py
@@ -25,26 +25,6 @@ class Application(BaseApplication):
             raise ValueError('Backend "{0}" is not registered!'.format(backend))
         self.backend = registry.backends[backend]
 
-    def __getitem__(self, key):
-        """Find the specified dialog of the application"""
-        # delegate searching functionality to self.window()
-        # return self.window(best_match=key)
-        return None
-
-    def __getattribute__(self, attr_name):
-        """Find the specified dialog of the application"""
-        if attr_name in ['__dict__', '__members__', '__methods__', '__class__']:
-            return object.__getattribute__(self, attr_name)
-
-        if attr_name in dir(self.__class__):
-            return object.__getattribute__(self, attr_name)
-
-        if attr_name in self.__dict__:
-            return self.__dict__[attr_name]
-
-        # delegate all functionality to item access
-        return self[attr_name]
-
     def start(self, cmd_line, timeout=None, retry_interval=None,
               create_new_console=False, wait_for_idle=True, work_dir=None):
         command_line = shlex.split(cmd_line)
