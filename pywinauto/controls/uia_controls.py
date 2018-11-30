7b6bed9613cc9c9162762cd7c8b6c32e3b1e17ed
Denis Nebytov <nebytov.d@gmail.com>
2018-12-01 01:31:40 +0300

    fix test

--- pywinauto/controls/uia_controls.py
+++ pywinauto/controls/uia_controls.py
@@ -952,7 +952,7 @@ class ToolbarWrapper(uiawrapper.UIAWrapper):
             texts = [c.window_text() for c in cc]
         else:
             cc = self.children()
-            texts = [c.text() for c in cc]
+            texts = [c.window_text() for c in cc]
 
         if isinstance(button_identifier, six.string_types):
             self.actions.log('Toolbar buttons: ' + str(texts))
