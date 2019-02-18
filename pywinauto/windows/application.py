a46733df47a5f4e4cc0dc7b8c9f741700e58c8d7
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2019-02-19 01:48:06 +0300

    Fix missing import for deprecated methods.

--- pywinauto/windows/application.py
+++ pywinauto/windows/application.py
@@ -93,8 +93,7 @@ from ..timings import Timings, wait_until, TimeoutError, wait_until_passes
 from ..sysinfo import is_x64_Python
 from ..base_application import AppStartError, ProcessNotFoundError, AppNotConnected, BaseApplication, \
     WindowSpecification
-from . import deprecated
-
+from .. import deprecated
 
 # Display User and Deprecation warnings every time
 for warning in (UserWarning, PendingDeprecationWarning):
