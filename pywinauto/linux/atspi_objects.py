a3ce83b903aeb84d1655bebd2d8303059f831a54
airelil <assotut@gmail.com>
2019-07-13 14:53:07 +1200

    atspi_objects: fix codacy warnings, add more docstrings

--- pywinauto/linux/atspi_objects.py
+++ pywinauto/linux/atspi_objects.py
@@ -7,13 +7,18 @@ from functools import wraps
 
 from ..backend import Singleton
 
+
 class CtypesEnum(object):
+
+    """Base class for Enums"""
+
     @classmethod
     def from_param(cls, obj):
         return int(obj)
 
 
 class RECT(Structure):
+
     """Wrap the RECT structure and add extra functionality"""
 
     _fields_ = [
@@ -434,7 +439,9 @@ ATSPI_ROLE_COUNT = 126
 
 @six.add_metaclass(Singleton)
 class IATSPI(object):
-    """ Python wrapper around C functions from atspi library"""
+
+    """ Python wrapper around C functions from ATSPI library"""
+
     LIB = "libatspi"
     DEFAULT_LIB_NAME = "libatspi.so"
 
@@ -495,6 +502,9 @@ class IATSPI(object):
 
 
 class GLIB(IATSPI):
+
+    """ Python wrapper around C functions from GLib library"""
+
     LIB = "libglib-2.0.so"
 
 
@@ -548,6 +558,9 @@ def _ghash2dic(ghash):
 
 
 class AtspiAccessible(object):
+
+    """Access to ATSPI Accessible Interface """
+
     get_desktop = IATSPI().get_iface_func("atspi_get_desktop")
     get_desktop.argtypes = [c_int]
     get_desktop.restype = POINTER(_AtspiAccessible)
@@ -634,6 +647,9 @@ class AtspiAccessible(object):
 
 
 class AtspiComponent(object):
+
+    """Access to ATSPI Component Interface """
+
     _contains = IATSPI().get_iface_func("atspi_component_contains")
     _contains.argtypes = [POINTER(_AtspiComponent), c_int, c_int, _AtspiCoordType, POINTER(POINTER(_GError))]
     _contains.restype = c_bool
@@ -729,6 +745,9 @@ class AtspiComponent(object):
 
 
 class AtspiStateSet(object):
+
+    """Access to ATSPI StateSet Interface """
+
     _new = IATSPI().get_iface_func("atspi_state_set_new")
     _new.argtypes = [POINTER(_GArray)]
     _new.restype = POINTER(_AtspiStateSet)
@@ -775,6 +794,9 @@ class AtspiStateSet(object):
 
 
 class AtspiAction(object):
+
+    """Access to ATSPI Action Interface """
+
     _get_action_description = IATSPI().get_iface_func("atspi_action_get_action_description")
     _get_action_description.argtypes = [POINTER(_AtspiAction), c_int, POINTER(POINTER(_GError))]
     _get_action_description.restype = c_char_p
@@ -854,6 +876,9 @@ class AtspiAction(object):
 
 
 class AtspiText(object):
+
+    """Access to ATSPI Text Interface """
+
     _get_character_count = IATSPI().get_iface_func("atspi_text_get_character_count")
     _get_character_count.argtypes = [POINTER(_AtspiText), POINTER(POINTER(_GError))]
     _get_character_count.restype = c_int
@@ -967,6 +992,9 @@ class AtspiText(object):
 
 
 class AtspiEditableText(object):
+
+    """Access to ATSPI Editable Interface """
+
     _set_text_contents = IATSPI().get_iface_func("atspi_editable_text_set_text_contents")
     _set_text_contents.argtypes = [POINTER(_AtspiEditableText), c_char_p, POINTER(POINTER(_GError))]
     _set_text_contents.restype = c_bool
@@ -1004,6 +1032,9 @@ class AtspiEditableText(object):
 
 
 class AtspiValue(object):
+
+    """Access to ATSPI Value Interface """
+
     _get_minimum_value = IATSPI().get_iface_func("atspi_value_get_minimum_value")
     _get_minimum_value.argtypes = [POINTER(_AtspiValue), POINTER(POINTER(_GError))]
     _get_minimum_value.restype = c_double
@@ -1050,9 +1081,7 @@ class AtspiValue(object):
 
 class AtspiDocument(object):
 
-    """
-    Access to ATSPI Document Interface
-    """
+    """Access to ATSPI Document Interface """
 
     _get_locale = IATSPI().get_iface_func("atspi_document_get_locale")
     _get_locale.argtypes = [POINTER(_AtspiDocument), POINTER(POINTER(_GError))]
