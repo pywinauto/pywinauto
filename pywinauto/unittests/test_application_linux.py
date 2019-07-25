acd79e507c1f753a8220821e7eb990c5ee4511d1
airelil <assotut@gmail.com>
2019-07-25 20:27:46 +1200

    Use system python3 for running gtk_example.py

--- pywinauto/unittests/test_application_linux.py
+++ pywinauto/unittests/test_application_linux.py
@@ -23,10 +23,6 @@ def _test_app():
     return os.path.join(test_folder, app_name)
 
 
-def _test_app_cmd_line():
-    return "python3.4 {}".format(_test_app())
-
-
 sys.path.append(".")
 
 if sys.platform != 'win32':
@@ -61,13 +57,13 @@ if sys.platform != 'win32':
             """test start() works correctly"""
             app = Application()
             self.assertEqual(app.process, None)
-            app.start(_test_app_cmd_line())
+            app.start(_test_app())
             self.assertNotEqual(app.process, None)
             app.kill()
 
         def test_connect_by_pid(self):
             """Create application wia subprocess then connect it to Application"""
-            subprocess_app = subprocess.Popen(['python3.4', _test_app()], stdout=subprocess.PIPE, shell=False)
+            subprocess_app = subprocess.Popen(_test_app().split(), stdout=subprocess.PIPE, shell=False)
             time.sleep(1)
             app = Application()
             app.connect(process=subprocess_app.pid)
@@ -76,30 +72,30 @@ if sys.platform != 'win32':
 
         def test_connect_by_path(self):
             """Create application wia subprocess then connect it to Application by application name"""
-            subprocess_app = subprocess.Popen(['python3.4', _test_app()], stdout=subprocess.PIPE, shell=False)
+            subprocess_app = subprocess.Popen(_test_app().split(), stdout=subprocess.PIPE, shell=False)
             time.sleep(1)
             app = Application()
-            app.connect(path='python3.4 {}'.format(_test_app()))
+            app.connect(path=_test_app())
             self.assertEqual(app.process, subprocess_app.pid)
             app.kill()
 
         def test_get_cpu_usage(self):
             app = Application()
-            app.start(_test_app_cmd_line())
+            app.start(_test_app())
             time.sleep(1)
             self.assertGreater(app.cpu_usage(), 0)
             app.kill()
 
         def test_is_process_running(self):
             app = Application()
-            app.start(_test_app_cmd_line())
+            app.start(_test_app())
             time.sleep(1)
             self.assertTrue(app.is_process_running())
             app.kill()
 
         def test_killed_app_not_running(self):
             app = Application()
-            app.start(_test_app_cmd_line())
+            app.start(_test_app())
             time.sleep(1)
             app.kill()
             time.sleep(1)
@@ -107,14 +103,14 @@ if sys.platform != 'win32':
 
         def test_kill_killed_app(self):
             app = Application()
-            app.start(_test_app_cmd_line())
+            app.start(_test_app())
             time.sleep(1)
             app.kill()
             time.sleep(1)
             self.assertTrue(app.kill())
 
         def test_kill_connected_app(self):
-            subprocess_app = subprocess.Popen(['python3.4', _test_app()], stdout=subprocess.PIPE, shell=False)
+            subprocess_app = subprocess.Popen(_test_app().split(), stdout=subprocess.PIPE, shell=False)
             time.sleep(1)
             app = Application()
             app.connect(process=subprocess_app.pid)
