64a82235b3245bcfd9d838ecd66dd749b71a12ef
airelil <assotut@gmail.com>
2019-08-11 11:20:44 +1200

    Stabilize TreeViewAdditionalTestCases.testCheckBoxes
    (cherry picked from commit cd9967a885219e67e9c65d0071f3fc934f9333e3)

--- pywinauto/unittests/test_common_controls.py
+++ pywinauto/unittests/test_common_controls.py
@@ -716,6 +716,7 @@ class TreeViewAdditionalTestCases(unittest.TestCase):
     def testCheckBoxes(self):
         """Make sure tree view item method is_checked() works as expected"""
         self.dlg.set_focus()
+        self.dlg.TVS_CHECKBOXES.uncheck_by_click()
         self.dlg.TVS_CHECKBOXES.check_by_click()
         birds = self.ctrl.get_item(r'\Birds')
         self.ctrl.set_focus() # to make sure focus is not lost by any accident event
