254c26e3e0bf718f0ebfdbfa308b1a095eb70a6c
airelil <assotut@gmail.com>
2019-11-01 22:04:33 +1300

    Silence flake8 warnings

--- pywinauto/application.py
+++ pywinauto/application.py
@@ -31,9 +31,9 @@
 
 import sys
 
-from .base_application import WindowSpecification
+from .base_application import WindowSpecification  # noqa: F401
 
 if sys.platform == 'win32':
-    from .windows.application import Application
+    from .windows.application import Application  # noqa: F401
 else:
-    from .linux.application import Application
+    from .linux.application import Application  # noqa: F401
