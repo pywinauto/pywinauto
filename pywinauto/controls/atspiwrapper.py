f1ee95e40aeeff5c473914dd6187fb22af5da7dd
airelil <assotut@gmail.com>
2020-01-18 17:13:49 +1300

    issue #873: initial docs for atspi backend

--- pywinauto/controls/atspiwrapper.py
+++ pywinauto/controls/atspiwrapper.py
@@ -29,7 +29,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-"""Basic wrapping of UI Automation elements"""
+"""Basic wrapping of Linux ATSPI elements"""
 
 from __future__ import unicode_literals
 from __future__ import print_function
