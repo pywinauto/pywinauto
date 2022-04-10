967ac1582df846f7358da4eec84f843f9c12bbb8
dariacher <32611360+dariacher@users.noreply.github.com>
2022-04-10 13:25:56 +0300

    delete useless import and join

--- pywinauto/unittests/macos/test_ax_wrapper.py
+++ pywinauto/unittests/macos/test_ax_wrapper.py
@@ -32,18 +32,15 @@
 
 """Tests for mac os AxWrapper"""
 
-import os
-import sys
-import unittest
 
 import sys
 import os
-import time
 import unittest
+
+
 if sys.platform == 'darwin':
     root_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     sys.path.append(root_dir)
-    os.path.join
     from pywinauto.macos.ax_element_info import AxElementInfo
     from pywinauto.macos.application import Application
     from pywinauto.macos.macos_structures import AX_POINT
