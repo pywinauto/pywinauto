455adf2ac26bc10da4abe298b335e81e4e85b658
eltio <i.n.1998@inbox.ru>
2022-05-04 13:59:13 +0300

    remove extra code for adding pid to criteria

--- pywinauto/base_application.py
+++ pywinauto/base_application.py
@@ -480,10 +480,7 @@ class WindowSpecification(object):
 
         new_item = WindowSpecification(self.criteria[0], allow_magic_lookup=self.allow_magic_lookup)
         new_item.criteria.extend(self.criteria[1:])
-        if self.app is not None:
-            criteria['pid'] = self.app.process
         new_item.criteria.append(criteria)
-
         return new_item
 
     def __getitem__(self, key):
