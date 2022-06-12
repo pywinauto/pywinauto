ab0baddb6e3abd9aa85d015944c88ffb89c47e57
dariacher <32611360+dariacher@users.noreply.github.com>
2022-06-12 18:01:42 +0300

    fx

--- pywinauto/unittests/windows/test_uiawrapper.py
+++ pywinauto/unittests/windows/test_uiawrapper.py
@@ -2019,8 +2019,8 @@ if UIA_support:
         def test_button_access_mfc(self):
             """Test getting access to buttons on Toolbar for MFC demo"""
             # Read a first toolbar with buttons: "File, View, Help"
-            self.assertEqual(self.menu_bar.button_count(), 4)
-            self.assertEqual(self.toolbar.button_count(), 11)
+            self.assertEqual(self.menu_bar.button_count(), 3)
+            self.assertEqual(self.toolbar.button_count(), 8)
 
             # Test if it's in writable properties
             props = set(self.menu_bar.get_properties().keys())
@@ -2037,9 +2037,9 @@ if UIA_support:
 
         def test_texts(self):
             """Test method .texts() for MFC Toolbar"""
-            self.assertEqual(self.menu_bar.texts(), [u'File', u'View', u'Help', u'Help'])
-            self.assertEqual(self.toolbar.texts(), [u'New', u'Open', u'Save', u'Save',
-                u'Cut', u'Copy', u'Paste', u'Paste', u'Print', u'About', u'About'])
+            self.assertEqual(self.menu_bar.texts(), [u'File', u'View', u'Help'])
+            self.assertEqual(self.toolbar.texts(), [u'New', u'Open', u'Save',
+                                                    u'Cut', u'Copy', u'Paste', u'Print', u'About'])
 
 
     class TreeViewWpfTests(unittest.TestCase):
@@ -2260,6 +2260,39 @@ if UIA_support:
             self.assertEqual(self.another_app.MouseButtonTester.has_focus(), False)
             # our window has been brought to the focus (clickable point exists)
             self.assertEqual(self.dlg.element_info.element.GetClickablePoint()[-1], 1)
+
+    class ToolBarTests(unittest.TestCase):
+        def setUp(self):
+            """Set some data and ensure the application is in the state we want"""
+            _set_timings()
+
+            test_folder = os.path.join(os.path.dirname(os.path.dirname(
+                os.path.dirname(os.path.abspath(__file__)))), r"apps/WinForms_samples")
+            self.toolbar_test = os.path.join(test_folder, "ToolBarTest.exe")
+
+            # start the application
+            self.app = Application(backend='uia')
+            self.app = self.app.start(self.toolbar_test)
+
+            self.dlg = self.app.ToolBarTest.find()
+            self.another_app = None
+
+        def tearDown(self):
+            """Close the application after tests"""
+            self.app.kill()
+            if self.another_app:
+                self.another_app.kill()
+                self.another_app = None
+
+        def test_toolbar_select(self):
+            self.dlg.toolbar_select('File->TestItem->Exit')
+            self.assertEqual(self.app.dlg.not_exists(), True)
+
+        def test_item_by_path(self):
+            toolbar = self.app.dlg.toolStrip1.find()
+            self.assertEqual(toolbar.item_by_path('#2->#1'), toolbar.item_by_path('Edit->Button2'))
+            self.assertEqual(toolbar.item_by_path('Edit->#0'), toolbar.item_by_path('Edit->Button1'))
+            self.assertEqual(toolbar.item_by_path('#2->Button3'), toolbar.item_by_path('Edit->Button3'))
 
 
 if __name__ == "__main__":
