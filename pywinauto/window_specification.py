6036a45f6436491bc103fa3620f89a347eef56fc
Jun Komoda <45822440+junkmd@users.noreply.github.com>
2023-08-29 09:08:16 +0900

    fix typo

--- pywinauto/window_specification.py
+++ pywinauto/window_specification.py
@@ -29,7 +29,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-"""Provides a object for finding a window or control"""
+"""Provides an object for finding a window or control"""
 
 from __future__ import print_function
 
