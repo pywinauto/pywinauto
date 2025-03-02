f56969f83a0f33379b1d035eb5c1c8ce9b2d0f3b
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2025-03-02 12:35:29 +0300

    Use light-weight AppVeyor image and install newer Python on top of it

--- ci/runTestsuite.ps1
+++ ci/runTestsuite.ps1
@@ -39,7 +39,7 @@ function run {
     }
 
     $results = "results.xml"
-    $ignored = "--ignore=testall.py" # " --ignore=test_application_linux.py --ignore-glob=test_*atspi*.py"
+    $ignored = "--ignore=testall.py --ignore=test_application_linux.py --ignore-glob=test_*atspi*.py"
     pytest --junit-xml=$results --tb=native --capture=no --show-capture=stdout $faulthandler_opt -v --verbosity=3 --cache-clear --durations=15 $ignored --log-level=DEBUG --cov-report html:Coverage_report --cov=pywinauto pywinauto\unittests\test_uiawrapper.py
     $success = $?
     Write-Output "result code of pytest: $success"
