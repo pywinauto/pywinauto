9ed8650d6987a3aa92784f7aa8c18b43a034f105
Jun Komoda <45822440+junkmd@users.noreply.github.com>
2025-03-03 00:10:26 +0000

    test enthought/comtypes#826

--- ci/install.ps1
+++ ci/install.ps1
@@ -86,7 +86,7 @@ function UpdateConda ($python_home) {
 
 function InstallComtypes ($python_home) {
     $pip_path = $python_home + "\Scripts\pip.exe"
-    $args = "install comtypes"
+    $args = "install https://github.com/junkmd/comtypes/archive/refs/heads/server_connectionpoints.zip"
     Start-Process -FilePath "$pip_path" -ArgumentList $args -Wait -Passthru
 }
 
